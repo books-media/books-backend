9e687500ec43e5f13608de50b41970dd155f8350
Nicole Kalbfleisch <ni.ka.50@hotmail.com>
2019-11-06 11:56:05 -0800

    spring init files

---
+++ src/main/java/com/booksmedia/booksmedia/BooksMediaApplication.java
@@ -0,0 +1,13 @@
+package com.booksmedia.booksmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booksmedia/booksmedia/BooksMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksmedia.booksmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
